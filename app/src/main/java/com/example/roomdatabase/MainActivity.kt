49e296640ee8a278482a8f870b55b470a1ee2a0a
Nababmithun <monermithun789@gmail.com>
2021-04-09 00:28:16 +0600

    RoomDatabase

---
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.roomdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.room.Room
+import com.example.roomdatabase.db.User
+import com.example.roomdatabase.db.UserDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    lateinit var db: UserDatabase
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        db = Room.databaseBuilder(applicationContext, UserDatabase::class.java, "database-name")
+                .build()
+
+        val user1 = User("James", "Bond", 35, true)
+        val user2 = User("Alice", "Smith", 28, true)
+        val user3 = User("Michael", "M.", 25, false)
+
+        GlobalScope.launch {
+            db.userDao().deleteAll()
+            db.userDao().insert(user1, user2, user3)
+
+            displayUsers()
+        }
+    }
+
+    private suspend fun displayUsers() {
+        val users = db.userDao().getAllUsers()
+        var displayText = ""
+        for (user in users) {
+            displayText += "${user.name} ${user.lastName} - ${user.age}\n"
+        }
+        display.text = displayText
+    }
+} lateinit var db: UserDatabase
